bc6d9348d2072f64b5c11a48435419ee957bf8a3
ssddyy97 <ssddjj97@gmail.com>
2025-04-07 10:41:14 +0900

    Update 숫자 암호 변환기 최종본.c

--- 숫자 암호 변환기 최종본.c
+++ 숫자 암호 변환기 최종본.c
@@ -1,62 +1,54 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 
 void main()
 {
-    int a;   
+    int a;
+
     printf("숫자를 입력하세요.\n");
     scanf("%d", &a);
 
-
-    switch(a)
-    {   
- 
+    switch (a)
+    {
         case 0:
             printf("A\n");
             break;
-                
+
         case 1:
             printf("B\n");
             break;
-                
+
         case 2:
             printf("C\n");
-            break;    
+            break;
+
         case 3:
             printf("D\n");
             break;
-                
+
         case 4:
             printf("E\n");
             break;
-                
+
         case 5:
             printf("F\n");
             break;
-                
+
         case 6:
             printf("G\n");
             break;
-                
+
         case 7:
             printf("H\n");
             break;
-                
+
         case 8:
             printf("I\n");
             break;
-                
+
         case 9:
             printf("J\n");
             break;
-                
+
         default:
             printf("잘못된 입력입니다");
     }
