782edf74ecfcfbae8933ad074724a4c032c19811
ssddyy97 <ssddjj97@gmail.com>
2025-04-08 15:02:54 +0900

    Add files via upload

---
+++ 1부터 n까지 출력하기.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+void main()
+
+{  
+    int num;
+    printf("숫자를 입력하세요 : ");
+    scanf("%d", &num);
+    int i = 0;
+    
+    while(i < num){
+    i++;
+    printf("%d ",i);
+        
+    }
+}
---
+++ 역피라미드 만들기.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+void main()
+
+{  
+    int num;
+    printf("값을 입력하세요:");
+    scanf("%d", &num);
+    for (int i = 0; i < num; i++){
+        for (int j = num - i; j > 0; j--){
+            printf("*");
+        }
+        
+    printf("\n");
+    } 
+}
