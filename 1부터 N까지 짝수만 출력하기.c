c9f4d92c41c0f86388936234eb06ad5831a10aca
ssddyy97 <ssddjj97@gmail.com>
2025-04-09 11:26:38 +0900

    Update 1부터 N까지 짝수만 출력하기.c

--- 1부터 N까지 짝수만 출력하기.c
+++ 1부터 N까지 짝수만 출력하기.c
@@ -1,27 +1,19 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 
-void main()
-
-{  
+void main() {
     int num;
     printf("숫자를 입력하세요 : ");
     scanf("%d", &num);
+
     int i = 0;
-    
-    while(i < num){
-     i++;
-     if(i % 2 == 1){
-        continue;
-    }
-    printf("%d ",i);
-        
+
+    while (i < num) {
+        i++;
+
+        if (i % 2 == 1) {
+            continue;
+        }
+
+        printf("%d ", i);
     }
 }
