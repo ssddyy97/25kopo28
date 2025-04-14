dd6e9945cd0b64df4115b414cd430caa65666807
ssddyy97 <ssddjj97@gmail.com>
2025-04-14 14:57:36 +0900

    Update 과제 1.c

--- 과제 1.c
+++ 과제 1.c
@@ -15,23 +15,26 @@ struct group {
 void input_data(int i) {
     char *name = g.n[i], *sex = g.s[i], *hobby = g.h[i];
     int *age = &g.a[i];
+
     printf("\n[이름 나이 성별 취미]를 입력하세요:\n");
+
     while (1) {
-        char temp[10];
-        if (scanf("%9s", temp) != 1) continue;
-        if (strcmp(temp, "exit") == 0) {
-            g.a[i] = -1; 
-            return;
-        }
-        strcpy(name, temp);
-        if (scanf("%d %9s %9s", age, sex, hobby) != 3) {
+        if (scanf("%9s %d %9s %9s", name, age, sex, hobby) != 4) {
             printf("입력 형식이 잘못되었습니다. 다시 입력해주세요.\n");
-            while (getchar() != '\n'); continue;
-        } if (*age < 0) {
-            printf("나이는 음수가 될 수 없습니다. 다시 입력해주세요.\n"); continue;
-        } if (strcmp(sex, "m") != 0 && strcmp(sex, "f") != 0) {
-            printf("성별은 'm' 또는 'f'로 입력해야 합니다. 다시 입력해주세요.\n"); continue;
+            while (getchar() != '\n');
+            continue;
         }
+
+        if (*age < 0) {
+            printf("나이는 음수가 될 수 없습니다. 다시 입력해주세요.\n");
+            continue;
+        }
+
+        if (strcmp(sex, "m") != 0 && strcmp(sex, "f") != 0) {
+            printf("성별은 'm' 또는 'f'로 입력해야 합니다. 다시 입력해주세요.\n");
+            continue;
+        }
+
         break;
     }
 }
@@ -39,21 +42,28 @@ void input_data(int i) {
 void print_all(int count) {
     printf("\n 현재까지 입력된 정보:\n");
     for (int j = 0; j < count; j++) {
-        if (g.a[j] == -1) break; 
-        printf("%d. %s / %d / %s / %s\n", j + 1, g.n[j], g.a[j], g.s[j], g.h[j]);
+        printf("%d. %s / %d / %s / %s\n",
+               j + 1, g.n[j], g.a[j], g.s[j], g.h[j]);
+    }
+}
+
+void init_data() {
+    for (int i = 0; i < 3; i++) {
+        printf("%d. %s / %d / %s / %s\n",
+               i + 1, g.n[i], g.a[i], g.s[i], g.h[i]);
+    }
+}
+
+void start_input() {
+    for (int i = 3; i < MAX; i++) {
+        input_data(i);
+        print_all(i + 1);
     }
 }
 
 int main() {
-    int i = 0;
-    for (; i < 3; i++)
-        printf("%d. %s / %d / %s / %s\n", i + 1, g.n[i], g.a[i], g.s[i], g.h[i]);
-
-    while (i < MAX) {
-        input_data(i);
-        if (g.a[i] == -1) break;  
-        print_all(++i);
-    }
-
+    init_data();
+    start_input();
     return 0;
 }
+
