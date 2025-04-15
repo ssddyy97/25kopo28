5e1d41abce08fd8f848b9d0315ed435149e3d926
ssddyy97 <ssddjj97@gmail.com>
2025-04-15 15:41:32 +0900

    Update 학생정보공용체.c

--- 학생정보공용체.c
+++ 학생정보공용체.c
@@ -12,36 +12,47 @@ struct group {
 };
 
 union unist {
-    char h[MAX][10];
-    char e[MAX][10];
-} u = {{"soccer", "music", "movie"}}; 
-
+    char h[MAX][10];  
+    char e[MAX][10];  
+} u = {{"soccer", "music", "movie"}};
 
 void input_data(int i) {
-    char *name = g.n[i], *sex = g.s[i], *hobby = u.h[i];
-    char *etc = u.e[i];
-    int *age = &g.a[i];  
+    char *name = g.n[i], *sex = g.s[i], *hobby = u.h[i], *etc = u.e[i];
+    int *age = &g.a[i];
+    char temp[100];
 
-    printf("\n이름 나이 성별 취미 기타를 입력하세요:\n");
+  
+    while (1) {
+        printf("\n이름을 입력하세요: ");
+        fgets(temp, sizeof(temp), stdin);
+        if (sscanf(temp, "%9s", name) == 1) break;
+        printf("이름을 정확히 입력해주세요.\n");
+    }
+
+ 
+    while (1) {
+        printf("나이를 입력하세요: ");
+        fgets(temp, sizeof(temp), stdin);
+        if (sscanf(temp, "%d", age) == 1 && *age >= 0) break;
+        printf("올바른 나이를 입력해주세요.\n");
+    }
 
     while (1) {
-        if (scanf("%9s %d %9s %9s %9s", name, age, sex, hobby, etc) != 5) {
-            printf("입력 형식이 잘못되었습니다. 다시 입력해주세요.\n");
-            while (getchar() != '\n');
-            continue;
-        }
-        
-        if (*age < 0) {
-            printf("나이는 음수가 될 수 없습니다. 다시 입력해주세요.\n");
-            continue;
-        }
-        
-        if (strcmp(sex, "m") != 0 && strcmp(sex, "f") != 0) {
-            printf("성별은 'm' 또는 'f'로 입력해야 합니다. 다시 입력해주세요.\n");
-            continue;
-        }
-        break;
+        printf("성별을 입력하세요 (m/f): ");
+        fgets(temp, sizeof(temp), stdin);
+        if (sscanf(temp, "%9s", sex) == 1 && (strcmp(sex, "m") == 0 || strcmp(sex, "f") == 0)) break;
+        printf("성별은 'm' 또는 'f'만 입력 가능합니다.\n");
     }
+
+   
+    printf("취미를 입력하세요: ");
+    fgets(temp, sizeof(temp), stdin);
+    if (sscanf(temp, "%9s", hobby) != 1) strcpy(hobby, "");
+
+   
+    printf("기타를 입력하세요: ");
+    fgets(temp, sizeof(temp), stdin);
+    if (sscanf(temp, "%9s", etc) != 1) strcpy(etc, "");
 }
 
 void print_all(int count) {
@@ -68,4 +79,4 @@ int main() {
     init_data();
     start_input();
     return 0;
-} 
+}
